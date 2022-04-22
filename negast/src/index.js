d5f4aecd7752273a503e86a116ce7e2bf945137a
Rishabh Gupta <rishabhg.1996@gmail.com>
2022-04-23 00:15:52 +0530

    Rectified Index file error

--- negast/src/index.js
+++ negast/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
 import { Provider as ReduxProvider } from "react-redux";
-import { store } from "./Redux/store";
+import { store } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
