6d84b60a2e341d620cc3119ef9f8d4bc6bf98ad8
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-04-22 23:42:35 +0530

    made the navbar for all pages

--- negast/src/App.js
+++ negast/src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import { Homepage } from './components/homepage/homepage';
 import { Navbar } from './components/navbar/navbar';
+import { SellerPost } from './components/sellerPost/sellerPost';
 import SignIn from './components/signin/signin';
 import {AllRoutes} from "./routes/route"
 
 function App() {
   return (
     <div className="App">
-      <AllRoutes/>
+      {/* <AllRoutes/> */}
+      <SellerPost/>
     </div>
   );
 }
