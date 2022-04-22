b5da4994e69a3e78ef4860330d4e1ff97e933a7d
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-04-22 17:42:36 +0530

    Login & signup UI part with route

--- negast/src/routes/route.jsx
+++ negast/src/routes/route.jsx
@@ -1,10 +1,14 @@
 import {Products} from "../components/allProducts/allProducts"
 import { Routes, Route } from "react-router";
+import SignIn from "../components/signin/signin";
+import Signup from "../components/signup/signup";
 export const AllRoutes=()=>{
     return(
         <>
             <Routes>
                 <Route exact path ="/allProducts" element = {<Products/>}/>
+                <Route exact path ="/signin" element = {<SignIn />}/>
+                <Route exact path ="/signup" element = {<Signup />}/>
             </Routes>
         </>
     )
