b6269f04e2c4d7de0aa09b69a7c02a08a22bcd8b
Rishabh Gupta <rishabhg.1996@gmail.com>
2022-04-22 16:47:15 +0530

    Added Product Page

--- negast/src/routes/route.jsx
+++ negast/src/routes/route.jsx
@@ -1,8 +1,11 @@
-
+import {Products} from "../components/allProducts/allProducts"
+import { Routes, Route } from "react-router";
 export const AllRoutes=()=>{
     return(
         <>
-        
+            <Routes>
+                <Route exact path ="/allProducts" element = {<Products/>}/>
+            </Routes>
         </>
     )
 }
