f2c1ba3366d3f25f462840e554cd6e8f20f28638
Rishabh Gupta <rishabhg.1996@gmail.com>
2022-04-23 20:15:44 +0530

    branding and category

--- negast/src/redux/products/action.jsx
+++ negast/src/redux/products/action.jsx
@@ -29,18 +29,27 @@ export const get_products_data = () => async (dispatch) => {
       const { data } = await axios.get(`https://negast.herokuapp.com/products/sort/high-low`);
       dispatch(get_products(data));
     } catch (error) {
-      console.log("Flat Error from Redux", error);
+      console.log("Product Error from Redux", error);
     }
   };
 
-//   export const FilterBlock = (value) => async (dispatch) => {
-//     try {
-//       const { data } = await axios.get(`https://afternoon-eyrie-36220.herokuapp.com/flat/block/?filter=${value}`);
-//       dispatch(get_flats(data));
-//     } catch (error) {
-//       console.log("Flat Error from Redux", error);
-//     }
-//   };
+  export const CategoriesBlock = (value) => async (dispatch) => {
+    try {
+      const { data } = await axios.get(`https://negast.herokuapp.com/products/category?category=${value}`);
+      dispatch(get_products(data));
+    } catch (error) {
+      console.log("Product Error from Redux", error);
+    }
+  };
+
+  export const BrandBlock = (value) => async (dispatch) => {
+    try {
+      const { data } = await axios.get(`https://negast.herokuapp.com/products/brand/?filter=${value}`);
+      dispatch(get_products(data));
+    } catch (error) {
+      console.log("Product Error from Redux", error);
+    }
+  };
 
 //   export const FilterType = (value) => async (dispatch) => {
 //     try {
